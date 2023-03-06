40aff67b5d034284365f60044c05910d731e7ea8
olesia1205 <olesia1205@yandex.ru>
2023-03-06 14:57:50 +0300

    ProtectedRoute

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import EditAvatarPopup from './EditAvatarPopup';
-// import { Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 function ProtectedRoute({element: Component, ...props}) {
   return (
-    // props.loggedIn ? <Component {...props} /> : <Navigate to="/sign-in" replace />
-    props.loggedIn ? <Component {...props} /> : <EditAvatarPopup />
+    props.loggedIn ? <Component {...props} /> : <Navigate to="/sign-in" replace />
   );
 }
 
