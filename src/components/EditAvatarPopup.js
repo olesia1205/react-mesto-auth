304029dcec2429ecd54de7bcafaa5a602643a1f1
olesia1205 <olesia1205@yandex.ru>
2023-03-18 22:15:46 +0300

    добавила валидацию EditAvatarPopup

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,15 +1,22 @@
-import React, { useRef } from 'react';
+import React, { useContext, useEffect } from 'react';
 import PopupWithForm from './PopupWithForm';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import useValidation from '../utils/Validation';
 
-function EditAvatarPopup({isOpen, onClose, onOverlayClick, ...props}) {
-  const avatarRef = useRef();
+function EditAvatarPopup({ isOpen, onClose, onOverlayClick, ...props }) {
+  const currentUser = useContext(CurrentUserContext);
+  const { values, errors, handleChange, resetValidation, isValid } = useValidation();
+
+  useEffect(() => {
+      resetValidation();
+  }, [isOpen, resetValidation]);
 
   function handleSubmit(evt) {
     evt.preventDefault();
-    props.onUpdateAvatar(`${avatarRef.current.value}`);
+    props.onUpdateAvatar(`${values.avatar}`);
   }
 
-  return(
+  return (
     <PopupWithForm
       name='avatar'
       title='Обновить аватар'
@@ -18,10 +25,23 @@ function EditAvatarPopup({isOpen, onClose, onOverlayClick, ...props}) {
       onClose={onClose}
       onSubmit={handleSubmit}
       onOverlayClick={onOverlayClick}
+      isDisabled={!isValid}
     >
       <>
-        <input className="popup__input popup__input_avatar-link" ref={avatarRef} type="url" name="avatar-link" placeholder="Ссылка на фото" defaultValue="" required/>
-        <span className="popup__input-error" id="avatar-link-error"/>
+        <div className="popup__input-container">
+          <input
+            className="popup__input popup__input_avatar-link"
+            type="url"
+            name="avatar"
+            value={values.avatar || ''}
+            onChange={handleChange}
+            placeholder="Ссылка на фото"
+            required
+          />
+          <span className={`popup__input-error ${!isValid && errors.avatar ? 'popup__input-error_active' : ''}`} id="avatar-error">
+            {errors.avatar || ''}
+          </span>
+        </div>
       </>
     </PopupWithForm>
   );
--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import PopupWithForm from './PopupWithForm';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 import useValidation from '../utils/Validation';
